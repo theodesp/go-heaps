828c1c4fdc6ded45f6aac96c71bbb60fc2eee4fb
radlinskii <radlinskiignacy@gmail.com>
2018-10-07 18:27:27 +0200

    ExtractMin needfix

--- fibonacci/heap.go
+++ fibonacci/heap.go
@@ -3,13 +3,16 @@
 package fibonacci
 
 import (
+	"fmt"
+
 	heap "github.com/theodesp/go-heaps"
 )
 
 // Heap is a implementation of Fibonacci heap.
 type Heap struct {
-	Min *Node
-	N   int
+	Min     *Node
+	N       int
+	maxRank int
 }
 
 // Node holds structure of nodes inside Fibonacci heap.
@@ -40,10 +43,7 @@ func (fh *Heap) Insert(x *Node) *Node {
 		x.right = x
 		fh.Min = x
 	} else {
-		fh.Min.left.right = x
-		x.right = fh.Min
-		x.left = fh.Min.left
-		fh.Min.left = x
+		addNode(fh.Min, x)
 
 		if fh.Min.Value.Compare(x.Value) > 0 {
 			fh.Min = x
@@ -73,4 +73,160 @@ func (fh *Heap) Union(fh2 *Heap) *Heap {
 	}
 	newFH.N = fh.N + fh2.N
 	return newFH
+}
+
+// ExtractMin extracts the node with minimum value from a heap
+// and returns pointer to this node.
+func (fh *Heap) ExtractMin() *Node {
+	z := fh.Min
+	if z != nil {
+		if c := z.child; c != nil {
+			c.parent = nil
+			fh.addRoot(c)
+			for r := c.right; r != c; r = r.right {
+				r.parent = nil
+				fh.addRoot(r)
+			}
+		}
+
+		z.left.right = z.right
+		z.right.left = z.left
+
+		if z == z.right {
+			fh.Min = nil
+		} else {
+			fh.Min = z.right
+			fh.consolidate()
+		}
+		fh.N--
+	}
+	return z
+}
+
+func (fh *Heap) consolidate() {
+	var degArr [10]*Node
+	flag := false
+	w := fh.Min
+	for {
+		if w == fh.Min {
+			if !flag {
+				flag = true
+			} else {
+				break
+			}
+		}
+		r := w.right
+		x := w
+		d := x.degree
+		for degArr[d] != nil {
+			y := degArr[d]
+			if x.Value.Compare(y.Value) > 0 {
+				xl := x.left
+				xr := x.right
+
+				x.left = y.left
+				x.right = y.right
+				y.left = xl
+				y.right = xr
+			}
+			fh.link(y, x)
+			degArr[d] = nil
+			d++
+		}
+		degArr[d] = x
+		w = r
+	}
+	fh.Min = nil
+	for i := 0; i < fh.maxRank; i++ {
+		if degArr[i] != nil {
+			fh.addRoot(degArr[i])
+		}
+	}
+}
+
+func (fh *Heap) link(y, x *Node) {
+	if y.parent != nil {
+		fmt.Println("node to link, cannot have a parent")
+	}
+
+	y.right.left = y.left
+	y.left.right = y.right
+
+	y.parent = x
+
+	if x.child == nil {
+		x.child = y
+		y.left = y
+		y.right = y
+	} else {
+		y.left = x.child.left
+		y.right = x.child
+		x.child.left.right = y
+		x.child.left = y
+	}
+
+	fh.maxRank++
+	y.mark = false
+}
+
+func (fh *Heap) addRoot(n *Node) {
+	if fh.Min == nil {
+		n.left = n
+		n.right = n
+		fh.Min = n
+	} else {
+		addNode(fh.Min, n)
+		if n.Value.Compare(fh.Min.Value) < 0 {
+			fh.Min = n
+		}
+	}
+}
+
+func addNode(h, x *Node) {
+	h.left.right = x
+	x.right = h
+	x.left = h.left
+	h.left = x
+}
+
+// Vis visualize
+func (fh Heap) Vis() {
+	if fh.Min == nil {
+		fmt.Println("<empty>")
+		return
+	}
+	var f func(*Node, string)
+	f = func(n *Node, pre string) {
+		pc := "│ "
+		for x := n; ; x = x.right {
+			if x.right != n {
+				fmt.Print(pre, "├─")
+			} else {
+				fmt.Print(pre, "└─")
+				pc = "  "
+			}
+			if x.child == nil {
+				fmt.Println("╴", x.Value)
+			} else {
+				fmt.Println("┐", x.Value)
+				f(x.child, pre+pc)
+			}
+			if x.right == n {
+				break
+			}
+		}
+	}
+	f(fh.Min, "")
+}
+
+func (n *Node) GetRight() *Node {
+	return n.right
+}
+
+func (n *Node) GetLeft() *Node {
+	return n.left
+}
+
+func (n *Node) GetChild() *Node {
+	return n.child
 }
