fb3364ec72c562d2f8773497528930d1629ef9fe
koneko096 <laser.survivor@gmail.com>
2018-10-12 22:25:29 +0700

    Implement split and merge

--- treap/treap.go
+++ treap/treap.go
@@ -7,26 +7,41 @@
 package treap
 
 import (
-    "fmt"
-    "math/rand"
+	"fmt"
+	"math/rand"
 
-    goheap "github.com/theodesp/go-heaps"
+	goheap "github.com/theodesp/go-heaps"
 )
 
 type Node struct {
-    Priority    goheap.Integer
-    Key         goheap.Item
-    Left, Right *Node
+	Priority    goheap.Integer
+	Key         goheap.Item
+	Left, Right *Node
 }
 
-func (this Node) CompareKey (than *Node) int {
-    return this.Key.Compare(than.Key)
+// Split treap into 2 treaps:
+// - All key in left treap <= key
+// - All key in right treap > key
+func split(t *Node, key goheap.Item) (*Node, *Node) {
+	var (
+		left, right *Node
+	)
+
+	if t == nil {
+		return nil, nil
+	} else if t.Key.Compare(key) != 1 {
+		t.Right, right = split(t.Right, key)
+		left := t
+		return left, right
+	} else {
+		left, t.Left = split(t.Left, key)
+		right := t
+		return left, right
+	}
 }
 
-func (this Node) ComparePriority (than *Node) int {
-    return this.Priority.Compare(than.Priority)
-}
-
+// Merge 2 treaps into one with condition:
+// max key on left treap is <= than min key on right treap
 func merge(x, y *Node) *Node {
 	if x == nil {
 		return y
@@ -34,35 +49,38 @@ func merge(x, y *Node) *Node {
 
 	if y == nil {
 		return x
-    }
-    
-    if x.ComparePriority(y) != 0 {
-        if x.ComparePriority(y) == 1 {
-            x, y = y, x
-        }
-
-        if x.CompareKey(y) == 1 {
-            x.Left = merge(x.Left, y)
-            return x
-        } else {
-            x.Right = merge(x.Right, y)
-            return x
-        }
-    }
-
-	if x.CompareKey(y) == 1 {
-		x, y = y, x
 	}
 
-	x.Left, x.Right = x.Right, x.Left
-	x.Left = merge(y, x.Left)
+	if x.Priority.Compare(y.Priority) == 1 {
+		x.Right = merge(x.Right, y)
+		return x
+	} else {
+		y.Left = merge(x, y.Left)
+		return y
+	}
+}
 
-	return x
+func (t *Node) insert(pnode *Node) *Node {
+	if t == nil {
+		return pnode
+	}
+
+	if pnode.Priority.Compare(t.Priority) == 1 {
+		pnode.Left, pnode.Right = split(t, pnode.Key)
+		return pnode
+	}
+
+	if t.Key.Compare(pnode.Key) != 1 {
+		t.Right = t.Right.insert(pnode)
+	} else {
+		t.Left = t.Left.insert(pnode)
+	}
+	return t
 }
 
 // Generate priority for new node.
-func generatePriority() goheap.Integer { 
-    return goheap.Integer(rand.Intn(65536)) 
+func generatePriority() goheap.Integer {
+	return goheap.Integer(rand.Intn(65536))
 }
 
 // Treap implementation.
@@ -71,70 +89,48 @@ type Treap struct {
 }
 
 // Init initializes or clears the Treap
-func (h *Treap) Init() *Treap {
-	return &Treap{}
-}
+func (h *Treap) Init() *Treap { return &Treap{} }
 
 // New returns an initialized Treap.
 func New() *Treap { return new(Treap).Init() }
 
 // Insert adds an item into the heap.
 func (h *Treap) Insert(v goheap.Item) goheap.Item {
-	h.Root = merge(&Node{
-        Priority: generatePriority(),
-        Key: v,
-	}, h.Root)
+	pnode := &Node{
+		Priority: generatePriority(),
+		Key:      v,
+	}
 
+	if h.Root == nil {
+		h.Root = pnode
+	} else {
+		h.Root = h.Root.insert(pnode)
+	}
 	return v
 }
 
 // DeleteMin deletes the minimum value and returns it.
 func (h *Treap) DeleteMin() goheap.Item {
-    v := h.Root
-    if v.Left == nil {
-        h.Root = v.Right
-        return v.Key
-    }
-
-    for ; v.Left.Left != nil; v = v.Left {}
-    
-    min := v.Left
-    v.Left = nil
-    return min.Key
-}
-
-func (n *Node) findMinNode() *Node {
-    if n == nil {
-        return nil
-    }
-
-    minL := n.Left.findMinNode()
-    minR := n.Right.findMinNode()
-
-    if minL == nil {
-		return minR
+	v := h.Root
+	if v.Left == nil {
+		h.Root = v.Right
+		return v.Key
 	}
 
-	if minR == nil {
-		return minL
-    }
+	for ; v.Left.Left != nil; v = v.Left {
+	}
 
-    if minL.CompareKey(minR) == 1 {
-        minL, minR = minR, minL
-    }
-
-    if minL.CompareKey(n) == 1 {
-        return n
-    } else {
-        return minL
-    }
+	min := v.Left
+	v.Left = nil
+	return min.Key
 }
 
 // FindMin finds the minimum value.
 func (h *Treap) FindMin() goheap.Item {
-    v := h.Root
-    for ; v.Left != nil; v = v.Left {}
-    return v.Key
+	v := h.Root
+	for ; v.Left != nil; v = v.Left {
+	}
+	return v.Key
 }
 
 // Clear removes all items from the heap.
@@ -147,15 +143,15 @@ type Printable interface {
 }
 
 func (h *Node) Print(lv int) {
-    if h == nil {
-        return
-    }
+	if h == nil {
+		return
+	}
 
-    h.Left.Print(lv + 1)
-    for i := 0; i < lv; i++ {
-        fmt.Print("-")
-    }
-    fmt.Printf("%d-",h.Priority)
-    h.Key.(Printable).Print()
-    h.Right.Print(lv + 1)
+	h.Left.Print(lv + 1)
+	for i := 0; i < lv; i++ {
+		fmt.Print("-")
+	}
+	fmt.Printf("%d-", h.Priority)
+	h.Key.(Printable).Print()
+	h.Right.Print(lv + 1)
 }
